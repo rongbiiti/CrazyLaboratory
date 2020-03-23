e2050ea045f7f13a866d3fe9cebab432665db39f
libra21 <11806015@stu.kbc.ac.jp>
2020-03-23 12:50:55 +0900

    アニメーションレイヤー追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -20,6 +20,8 @@ public class PlayerController : MonoBehaviour
 
     private bool flip = true;
 
+    Animator animator;  //アニメーション変数
+
     [SerializeField, CustomLabel("地面との当たり判定")] private ContactFilter2D filter2d;
     private bool isGrounded = true;
 
@@ -160,6 +162,8 @@ public class PlayerController : MonoBehaviour
         }
         PreCalcSpreadAngle();
 
+        animator = GetComponent<Animator>(); //アニメーション
+
         ShowListContentsInTheDebugLog(hmSpreadAngle);
     }
 
@@ -253,10 +257,26 @@ public class PlayerController : MonoBehaviour
 
         // 地面にいるとき
         if (isGrounded) {
+            animator.SetBool("JumpUp", false);
             rb.AddForce(new Vector2(playerManager.MoveForceMultiplier * (inputManager.MoveKey * playerManager.MoveSpeed - rb.velocity.x), rb.velocity.y));
 
-        // 空中にいるとき
+            if (inputManager.MoveKey != 0)
+            {
+                //Debug.Log("顔が消えた");
+                animator.SetBool("Run", true);
+                animator.SetBool("Stand", false);
+            }
+            else if (inputManager.MoveKey == 0 && rb.velocity.x <= 4f && -4f <= rb.velocity.x)
+            {
+                animator.SetBool("Stand", true);
+                animator.SetBool("Run", false);
+            }
+            // 空中にいるとき
         } else {
+            animator.SetBool("JumpUp", true);
+            animator.SetBool("Run", false);
+            animator.SetBool("Stand", false);
+
             // ジャンプキーが話されたらジャンプ中でないことにする
             if (inputManager.JumpKey == 0) {
                 isJumping = false;
@@ -287,6 +307,8 @@ public class PlayerController : MonoBehaviour
             
             // ジャンプキーを押し続けていられる時間をへらす
             jumpTimeCounter -= Time.deltaTime;
+
+            animator.SetBool("Run", false);
 
             // ジャンプキーを押し続けている間は通常のジャンプパワー軽減率がはたらく
             if (inputManager.JumpKey == 2) {
