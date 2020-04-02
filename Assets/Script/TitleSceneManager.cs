b2cf21487cd7c347d1b867a8582742f609597603
rongbiiti <11807078@stu.kbc.ac.jp>
2020-04-02 12:32:16 +0900

    ロゴを使ってタイトルシーン作ってみた

---
+++ Assets/Script/TitleSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TitleSceneManager : MonoBehaviour {
+
+    private bool isAnyKeyPress;
+
+    void Update () {
+		if(Input.anyKey && !isAnyKeyPress) {
+            FadeManager.Instance.LoadScene("zikken3_Stage1", 2f);
+            SoundManagerV2.Instance.PlaySE(16);
+            isAnyKeyPress = true;
+        }
+	}
+}
