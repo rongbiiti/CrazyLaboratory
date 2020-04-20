2d19995caf8d7f73b7c33a84ba6614d680b4b284
rongbiiti <11807078@stu.kbc.ac.jp>
2020-04-20 11:11:18 +0900

    カマキリの上に来たときにカメラが下を注視するアレを値が変えられるようにした　マイナスにすると逆に上を注視できる

--- Assets/Script/FocasUnder.cs
+++ Assets/Script/FocasUnder.cs
@@ -3,20 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FocasUnder : MonoBehaviour {
+public class FocasUnder : MonoBehaviour
+{
+    [Header("12でキャラの頭が画面の上端につくくらいになります")]
+    [Header("マイナスにすると逆に上を注視するようになります")]
+    [SerializeField, CustomLabel("カメラどのくらい下げるか")] private float _offset = 12f;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player")) {
-            GameObject cam = GameObject.Find("Main Camera");
-            cam.GetComponent<CameraController>().SetIsFocasUnder(true);
-        }
+        if (!other.CompareTag("Player")) return;
+        var cam = GameObject.Find("Main Camera");
+        cam.GetComponent<CameraController>().SetIsFocasUnder(true, _offset);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.CompareTag("Player")) {
-            GameObject cam = GameObject.Find("Main Camera");
-            cam.GetComponent<CameraController>().SetIsFocasUnder(false);
-        }
+        if (!other.CompareTag("Player")) return;
+        var cam = GameObject.Find("Main Camera");
+        cam.GetComponent<CameraController>().SetIsFocasUnder(false);
     }
 }
