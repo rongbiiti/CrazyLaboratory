b66adb5b2a669347397e73ecd03168f693e58e29
rongbiiti <11807078@stu.kbc.ac.jp>
2020-05-21 15:01:53 +0900

    RestartPointで2回目以降の復活で色々おかしくなる不具合修正　ガレキ、回復薬、吊るされた回復箱は大丈夫になった

---
+++ Assets/Script/RestartManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RestartManagerTest : MonoBehaviour {
+
+    [System.Serializable]
+    public class EnemySaveData
+    {
+        public GameObject[] _enemys;
+        public bool[] _isInactivate;
+    }
+
+    
+}
