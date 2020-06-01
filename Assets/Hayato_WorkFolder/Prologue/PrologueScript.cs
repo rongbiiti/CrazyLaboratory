63fe2e19c5d666808a0e04b2b4124fe5180bf576
rongbiiti <11807078@stu.kbc.ac.jp>
2020-06-01 10:39:40 +0900

    トータルリザルト画面作った　Hayato_WorkFolder\TotalResultScene\TotalResultScene

---
+++ Assets/Hayato_WorkFolder/Prologue/PrologueScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologueScript : MonoBehaviour {
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private IEnumerator Prologue()
+    {
+        yield return null;
+    }
+}
